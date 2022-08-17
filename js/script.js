3ebbb838538bc82e08a67b36c1ae61538ad388ac
Svitlana Bilous <svitlana.bilous0818@gmail.com>
2022-08-17 16:34:14 +0300

    scroll progress

--- js/script.js
+++ js/script.js
@@ -15,3 +15,15 @@ if (reviewsSwiper) {
     },
   });
 }
+
+// scroll progress
+const readingProgress = document.querySelector("#reading-progress-fill");
+document.addEventListener("scroll", function (e) {
+  let w =
+    ((document.body.scrollTop || document.documentElement.scrollTop) /
+      (document.documentElement.scrollHeight -
+        document.documentElement.clientHeight)) *
+    100;
+  readingProgress.style.setProperty("width", w + "%");
+});
+// scroll progress
